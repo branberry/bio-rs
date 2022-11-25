c2676f3ec6987a498f30c9534bddbbf06d042ddb
Brandon Ly <brandonly@lostcoding.com>
2022-11-24 20:20:42 -0600

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+
+import '../style.css';
+import '../App.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
